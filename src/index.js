083ee449eb46d1f16c8d9d26ef4c617950fe0de6
Oleg Afanasev <oleg.afanasev@rt.ru>
2020-06-18 10:22:56 +0300

    Сonfigure webpack and add Joomborton component

--- src/index.js
+++ src/index.js
@@ -2,4 +2,10 @@
 
 import init from './init';
 
+import 'bootstrap/js/dist/util';
+import 'bootstrap/js/dist/button';
+
+import 'bootstrap/dist/css/bootstrap-grid.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 init();
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,14 @@
+const path = require('path');
+const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: process.env.NODE_ENV || 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist')
+  },
   module: {
     rules: [
       {
@@ -11,11 +18,20 @@ module.exports = {
           loader: 'babel-loader',
         },
       },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: 'template.html',
+      template: 'index.html',
     }),
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      jquery: 'jquery',
+      'window.jquery': 'jquery'
+    })
   ],
 };
